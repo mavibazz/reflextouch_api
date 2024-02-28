c179bc36f7264caf0eff893d88988beea9a36fab
marvins <ajekiigbemarvellous@gmail.com>
2024-02-28 13:52:16 +0100

    connect

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const user = require("./routes/User")
 const login = require("./routes/Login")
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://react-ashy-three.vercel.app/",
     allowedHeaders:["Content-Type", "Authorization", "x-auth-token"],
     methods: ["GET", "POST", "PUT","PATCH", "DELETE"]
 }))
